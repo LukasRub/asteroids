bf0094d9e94a140f8c6ab2017a997e7a7d68315d
LukasRub <lukas.rubikas@gmail.com>
2025-05-11 09:32:33 +0300

    ch2 assignments

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
 from constants import *
+from player import Player
 
 import pygame
 
 
 def main():
     pygame.init()
+
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
 
+        updatable.update(dt)
+
         screen.fill("black")
+        for sprite in drawable:
+            sprite.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
